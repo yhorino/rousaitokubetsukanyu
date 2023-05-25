0ac4a43fd7e1efbc76c8e49603eea369e6facd88
HORINO-NOTE <y_horino@tmgt.co.jp>
2023-05-25 16:58:39 +0900

    事務組合RJC　マイページ　名前が長いと切れる
    ●名前の長さに合わせてフォントサイズを変える処理を追加
    　・17文字以上：12px
    　・19文字以上：11px
    　・21文字以上：10px

--- mypage/kaiinsyo.php
+++ mypage/kaiinsyo.php
@@ -101,6 +101,16 @@ $title="会員カード";
  /* 20220325 SFデータ入力完了までは2022/4/1～2023/3/31にする */
  if($Kanyunengappiwareki__c == '') $Kanyunengappiwareki__c = '2023-04-01';
  if($Kanyumankiwareki__c == '') $Kanyumankiwareki__c = '2024-03-31';
+ 
+ $name_len = mb_strlen($kanyusya['Name']);
+ $name_style = '';
+ if($name_len >20){
+  $name_style = ' style="font-size:8px; padding-top:0.3em;" ';
+ } else if($name_len >18){
+  $name_style = ' style="font-size:11px; padding-top:0.2em;" ';
+ } else if($name_len >16){
+  $name_style = ' style="font-size:12px; padding-top:0.1em;" ';
+ }
 ?>
 <div class="inner" style="padding: 0;">
 
@@ -112,7 +122,7 @@ $title="会員カード";
   <p id="roudouhokenbangou"><span id="title_roudouhokenbangou" class="kaiinsyo_title"><span>労</span><span>働</span><span>保</span><span>険</span><span>番</span><span>号</span></span><span><?php echo $bangou;?></span></p>
   <p id="jigyosyo"><span id="title_jigyosyo" class="kaiinsyo_title"><span>事</span><span>業</span><span>所</span><span>名</span></span><span style="font-size: 10px; padding: 0.3em 0;"><?php echo $jigyosyo;?></span></p>
   <p id="seiribangou"><span id="title_seiribangou" class="kaiinsyo_title"><span>整</span><span>理</span><span>番</span><span>号</span></span><span><?php echo $_GET['seiribangou'];?></span></p>
-  <p id="name"><span id="title_name" class="kaiinsyo_title"><span>氏</span><span>名</span></span><span><?php echo $kanyusya['Name'];?></span></p>
+  <p id="name"><span id="title_name" class="kaiinsyo_title"><span>氏</span><span>名</span></span><span <?php echo $name_style;?>><?php echo $kanyusya['Name'];?></span></p>
   <p id="hakkou"><span id="title_hakkou" class="kaiinsyo_title"><span>発</span><span>行</span><span>年</span><span>月</span><span>日</span></span><span><?php echo date('Y年m月d日', strtotime($Kanyunengappiwareki__c));?></span></p>
   <p id="kigen"><span id="title_kigen" class="kaiinsyo_title"><span>有</span><span>効</span><span>期</span><span>限</span></span><span><?php echo date('Y年m月d日', strtotime($Kanyumankiwareki__c));?></span></p>
  </div>
