f6b47382e6887218dbf8a9a8895d7b3f87d62df9
HORINO-NOTE <y_horino@tmgt.co.jp>
2023-07-24 12:48:54 +0900

    事務組合RJC　フォーム本番アップ
    ●毎月払い、短期加入
    　・フォーム修正点の反映とアップ
    　・団体則、誓約書の差し替え
    　　・フォーム確認画面の団体則、誓約書差し替え
    　　・申込後のメール（アシロボ）添付の団体則、誓約書差し替え
    　　・マイページダウンロードの団体則差し替え

--- mypage/download.php
+++ mypage/download.php
@@ -60,7 +60,7 @@ $title="各種ダウンロード・印刷";
    <td>02</td><td>特別加入証明書</td><td>2023/03/23</td><td><a href="kanyusya.php">閲覧・印刷</a></td><td>特別加入の証明書を閲覧・印刷できます。</td>
   </tr>
   <tr>
-   <td>03</td><td>団体則</td><td>2023/05/30</td><td><a href="dantaisoku.pdf">閲覧・印刷</a></td><td>労働保険事務組合RJCの団体則を閲覧・印刷できます。</td>
+   <td>03</td><td>団体則</td><td>2023/07/21</td><td><a href="dantaisoku.pdf">閲覧・印刷</a></td><td>労働保険事務組合RJCの団体則を閲覧・印刷できます。</td>
   </tr>
   <?php if($_SESSION['row']['kyotei36_URL__c'] != ''){ ?>
   <tr>
