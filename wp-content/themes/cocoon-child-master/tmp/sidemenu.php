cbbb61ddb6b4171614e4d759bfb2e89080aa604f
HORINO-NOTE <y_horino@tmgt.co.jp>
2024-03-15 09:49:00 +0900

    一人親方RJC、事務組合RJC、CCUS　スマホ電話ボタンのリンク先修正
    ・リンク先をお知らせページに変更

---
+++ wp-content/themes/cocoon-child-master/tmp/sidemenu.php
@@ -0,0 +1,87 @@
+<link rel="stylesheet" href="/wp-content/themes/cocoon-child-master/tmp/sidemenu.css" type="text/css">
+
+<nav id="common_sidemenu">
+ 
+ <div class="sm_outer jimu">
+  
+  <div class="sm_header">
+   
+   <div class="sm_logo">
+    <a href="/">
+     <div class="sm_logo_button_sp"><img src="https://www.xn--y5q0r2lqcz91qdrc.com/wp-content/uploads/2023/05/logo_jimukumiai.png" alt="労働保険事務組合RJC"></div>
+    </a>
+   </div>
+   
+   <div class="sm_spacer">
+   </div>
+   
+   <div class="sm_menuclose">
+    <a onclick="menu_slideout();">
+     <div class="sm_menuclose_button_pc"><i class="fas fa-times menuclose_icon"></i> <span class="menuclose_text">閉じる</span></div>
+     <div class="sm_menuclose_button_sp sp_button"><i class="fas fa-times menuclose_icon sp_icon"></i><span class="menuclose_text sp_text">閉じる</span></div>
+    </a>
+   </div>
+   
+  </div>
+  
+  <div class="sm_body">
+   <ul class="sm_list">
+     <li><a href="/koyohoken/">従業員の雇用保険（入社）</a></li>
+     <li><a href="/koyohoken/taisyoku.html">従業員の雇用保険（退職）</a></li>
+     <li><a href="/kumiai/">組合案内</a></li>
+     <li><a href="/sdgs/">SDGsの取り組み</a></li>
+     <li><a href="/about_tokka/">特別加入制度とは</a></li>
+     <li><a href="/youken/">特別加入の要件とは</a></li>
+     <li><a href="/kyufu/">保険給付について</a></li>
+     <li><a href="/rosaitokka/">建設業の労災保険と特別加入について</a></li>
+     <li><a href="/rosaikirikae/">労災保険の切替えについて</a></li>
+     <li><a href="/faq/">よくある質問</a></li>
+     <li><a href="/jimu_magazine/">事務組合マガジン</a></li>
+     <li><a href="/info/">お知らせ</a></li>
+   </ul>
+  </div>
+  
+  <div class="sm_footer">
+   
+   <div class="sm_footer_telbutton">
+    <div class="sm_tel">
+     <a href="tel:0120855865">
+      <div class="sm_tel_button_pc"><img src="/wp-content/uploads/2023/09/top_tel_s.png" alt="0120855865"></div>
+     </a>
+    </div>
+   </div>
+   
+   <div class="sm_footer_buttons">
+    <div class="sm_mypage">
+     <a href="/mypage/index.php">
+      <div class="sm_mypage_button_pc"><i class="fas fa-user-lock mypage_icon"></i> <span class="mypage_text">マイページログイン</span></div>
+     </a>
+    </div>
+
+    <div class="sm_form">
+     <a href="/form/mitsumori.php">
+      <div class="sm_form_button_pc"><i class="fas fa-calculator form_icon"></i> <span class="form_text">無料お見積り</span></div>
+     </a>
+    </div>
+   </div>
+   
+  </div>
+  
+ </div>
+ 
+</nav>
+
+<script>
+ jQuery(function($){
+  $('#common_sidemenu').addClass('hide');
+ });
+ function menu_slidein(){
+  jQuery('#common_sidemenu').removeClass('hide');
+  jQuery('#common_sidemenu').addClass('show');
+ }
+ function menu_slideout(){
+  jQuery('#common_sidemenu').removeClass('show');
+  jQuery('#common_sidemenu').addClass('hide');
+ }
+</script>
+
